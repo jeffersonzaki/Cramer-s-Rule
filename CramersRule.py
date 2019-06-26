77e5a7adfbd3bacb131f1fc7c7488684978ee0f5
Zaki Jefferson <zakirjefferson@gmail.com>
2019-06-26 08:47:00 -0400

    Add files via upload

---
+++ CramersRule.py
@@ -0,0 +1,15 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+d = float(input("Enter d: "))
+e = float(input("Enter e: "))
+f = float(input("Enter f: "))
+
+
+if (a*d - b*c == 0):
+    print("The equation has no solution")
+else:
+    x = (e*d-b*f)/(a*d-b*c)
+    y = (a*f-e*d)/(a*d-b*c)
+
+    print ("x=%s" % x, "y=%s" % y)
